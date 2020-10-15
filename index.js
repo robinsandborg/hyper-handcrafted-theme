f805034f56cb3020b8fcfb2d3ecf648abdffefa0
Robin Sandborg <robin.sandborg@gmail.com>
2020-10-15 10:37:11 +0200

    tweak bright colors
    Signed-off-by: Robin Sandborg <robin.sandborg@gmail.com>

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
-'use strict';
+'use strict'
 
-const backgroundColor = "#101d27";
-const foregroundColor = "#d1c4b6";
-const borderColor = "#25323d";
-const cursorColor = "#5eb231";
+const backgroundColor = "#101d27"
+const foregroundColor = "#d1c4b6"
+const borderColor = "#25323d"
+const cursorColor = "#5eb231"
 const accentColor = "#59be84"
-const padding = "12px 14px",
+const padding = "12px 14px"
+
 const colors = {
-  black: "#03131D",
-  red: "#fd8877",
-  green: "#5eb231",
-  yellow: "#d0ca6a",
-  blue: "#9aa5ff",
-  magenta: "#f585ca",
-  cyan: "#59be84",
-  white: "#d1c4b6",
-  lightBlack: "#53606d",
-  lightRed: "#ffa290",
-  lightGreen: "#79cd4b",
-  lightYellow: "#b3ae50",
-  lightBlue: "#b6bfff",
-  lightMagenta: "#ffa1e7",
-  lightCyan: "#76da9e",
-  lightWhite: "#eee0d1",
+      black: "#03131D",
+      red: "#fd8877",
+      green: "#5eb231",
+      yellow: "#b3ae50",
+      blue: "#9aa5ff",
+      magenta: "#f585ca",
+      cyan: "#59be84",
+      white: "#d1c4b6",
+      lightBlack: "#53606d",
+      lightRed: "#ffa290",
+      lightGreen: "#79cd4b",
+      lightYellow: "#d0ca6a",
+      lightBlue: "#b6bfff",
+      lightMagenta: "#ffa1e7",
+      lightCyan: "#76da9e",
+      lightWhite: "#eee0d1"
 }
 
 exports.decorateConfig = (config) => {
@@ -46,6 +47,6 @@ exports.decorateConfig = (config) => {
 			.tab_active:before {
 				border-color: ${borderColor};
 			}
-		`,
-  });
-};
+		`
+  })
+}
